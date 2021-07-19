a32cbefe570ae80e38b523cb141bc0dad6365ca6
Gil Amsalem <gil.amsalem@booking.com>
2021-07-20 01:55:46 +0300

    fix base docker

--- base-docker-publish.sh
+++ base-docker-publish.sh
@@ -6,7 +6,5 @@ COMPONENT=image-quality-base
 DOCKERFILE=Dockerfile.cpu
 VERSION=1.0.0
 
-cd image-quality-assessment/
 docker build --pull . -f $DOCKERFILE -t docker.jfrog.booking.com/projects/$PROJECT/$COMPONENT:$VERSION
 docker push docker.jfrog.booking.com/projects/$PROJECT/$COMPONENT:$VERSION
-cd ../
